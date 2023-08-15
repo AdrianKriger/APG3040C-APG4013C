82bc62bd1e95bc161701ffe6d468aa1f38a30f52
Adrian Kriger <59996720+AdrianKriger@users.noreply.github.com>
2023-08-15 14:20:50 +0200

    runCommit03

--- cptFlatsAquifer/trendSurface02.R
+++ cptFlatsAquifer/trendSurface02.R
@@ -1,20 +1,10 @@
-install.packages("sf")
-install.packages("gstat")
-install.packages("ggplot2")
-install.packages("gridExtra")
-install.packages("units")
-install.packages("terra")
+# Advanced Spatial Data Analysis ~ Interpolation Technics
+# arkriger - august 2023
 
 library(sf) # 'simple features' representations of spatial objects
 library(gstat) # geostatistics
-library(ggplot2) # Grammer of Graphics plots
-library(gridExtra) # arrange multiple ggplot graphics on one figure
 library(units) # units of measure
 library(terra) 
-
-
-setwd('C://Users/T0084076/Downloads/')
-file = 'cptFlatsAquifer_watertable4.txt'
 
 #-- import
 cfaq <- read.csv(file, header = 1, sep = ',', dec = '.') 
@@ -22,30 +12,28 @@ cfaq <- read.csv(file, header = 1, sep = ',', dec = '.')
 cfaq.sf <- st_as_sf(cfaq, coords=c("long", "lat"), crs = 4326) #wgs84
 #- transform to local crs
 cfaq.sf <- st_transform(cfaq.sf, crs = 32734) #utm 34s
-cfaq.sf$X <- st_coordinates(cfaq.sf)[, "X"]
-cfaq.sf$Y <- st_coordinates(cfaq.sf)[, "Y"]
+# Extract x and y coordinates from the geometry column
+cfaq$X <- st_coordinates(cfaq.sf)[, "X"]
+cfaq$Y <- st_coordinates(cfaq.sf)[, "Y"]
 head(cfaq ,3)
 
-#head(model.matrix(~st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2], 
-#                  data=cfaq))
-head(model.matrix(~X + Y, data=cfaq.sf))
+#head(model.matrix(~ st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2],data=cfaq))
+head(model.matrix(~X + Y, data=cfaq))
 
-
-#model.ts1 <- lm(cfaq$waterLevel ~ st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2])#, data=drop_units(cfaq))
-model.ts1 <- lm(waterLevel ~ X + Y, data=drop_units(cfaq.sf))
+#model.ts1 <- lm(cfaq$waterLevel ~ st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2], data=drop_units(cfaq))
+model.ts1 <- lm(waterLevel ~ X + Y, data=drop_units(cfaq))
 summary(model.ts1)
 
 #- summarize residuals (lack of fit)
 res.ts1 <- set_units(residuals(model.ts1), m); summary(res.ts1)
 
 #- histogram
-hist(res.ts1, breaks=16, main="Residuals from 1st-order trend", 
-     xlab="residual elevation (m)")
+hist(res.ts1, breaks=16, main="Residuals from 1st-order trend", xlab="residual elevation (m)")
 
 rug(res.ts1)
 range(res.ts1)
 
-max(abs(res.ts1))/median(cfaq.sf$waterLevel)*100
+max(abs(res.ts1))/median(cfaq$waterLevel)*100
 
 #- diagnostic plot of linear model
 par(mfrow=c(1,2))
@@ -53,7 +41,7 @@ plot(model.ts1, which=1:2)
 par(mfrow=c(1,1))
 
 #- residuals as postplot
-plot(cfaq.sf$Y ~ cfaq.sf$X, 
+plot(cfaq$Y ~ cfaq$X, 
      #st_coordinates(cfaq.sf)[,1], st_coordinates(cfaq.sf)[,2],
      cex=3*abs(res.ts1)/max(abs(res.ts1)),
      col=ifelse(res.ts1 > set_units(0, m), "green", "red"),
@@ -64,15 +52,16 @@ grid()
 
 cfaq.sf
 
-#head(model.matrix(~st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2] + I(st_coordinates(cfaq.sf)[,1]^2) + I(st_coordinates(cfaq.sf)[,2]^2)
+##- 2nd-order
+#head(model.matrix(~ st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2] + I(st_coordinates(cfaq.sf)[,1]^2) + I(st_coordinates(cfaq.sf)[,2]^2)
 #                  + I(st_coordinates(cfaq.sf)[,1]*st_coordinates(cfaq.sf)[,2]), data=cfaq))
-head(model.matrix(~ X + Y + I(X^2) + I(Y^2) + I(X*Y), data=cfaq.sf))
+head(model.matrix(~ X + Y + I(X^2) + I(Y^2) + I(X*Y), data=cfaq))
 
 #- fit the 2nd order
 #model.ts2 <- lm(cfaq$waterLevel ~ st_coordinates(cfaq.sf)[,1] + st_coordinates(cfaq.sf)[,2] + I(st_coordinates(cfaq.sf)[,2]^2) + I(st_coordinates(cfaq.sf)[,1]^2) + I(st_coordinates(cfaq.sf)[,2]*st_coordinates(cfaq.sf)[,1]), 
 #                data=drop_units(cfaq))
-model.ts2 <- lm(waterLevel ~ X + Y + I(X^2) + I(Y^2) + I(X*Y), 
-                data=drop_units(cfaq.sf))
+model.ts2 <- lm(waterLevel ~ X + Y + I(X^2) + I(Y^2) + I(X*Y), data=drop_units(cfaq))
+
 summary(model.ts2)
 
 #- compare the 1st and 2nd order models statistically
@@ -86,7 +75,7 @@ summary(res.ts2)
 hist(res.ts2, breaks=16, main="Residuals from 2nd-order trend", 
      xlab="residual elevation (m)")
 rug(res.ts2)
-max(abs(res.ts2))/median(cfaq.sf$waterLevel)
+max(abs(res.ts2))/median(cfaq$waterLevel)
 
 #- diagnostic plots
 par(mfrow=c(1,2))
@@ -97,63 +86,56 @@ summary(sres.ts2 <- rstandard(model.ts2))
 
 (ix <- which(sres.ts2 < -1))
 
-(cbind(actual=cfaq.sf[ix, "waterLevel"], fitted=fitted(model.ts2)[ix],
+(cbind(actual=cfaq[ix, "waterLevel"], fitted=fitted(model.ts2)[ix],
        residual=res.ts2[ix],
        std.res <- sres.ts2[ix]))
 
 #- display residuals as postplot
-plot(cfaq.sf$Y ~ cfaq.sf$X,
+plot(cfaq$Y ~ cfaq$X,
      #st_coordinates(cfaq.sf)[,2] ~ st_coordinates(cfaq.sf)[,1], 
      cex=3*abs(res.ts2)/max(abs(res.ts2)), 
      col=ifelse(res.ts2 > set_units(0, m), "green", "red"),
      xlab="E", ylab="N", asp=1,
      main="Residuals from 2nd-order trend",
-     sub="Positive: green; negative: red; black dots: severe over-predictions")
-points(cfaq.sf[ix, "Y"] ~ cfaq.sf[ix, "X"],        pch=20)
-       #st_coordinates(cfaq.sf)[ix, 'X'], st_coordinates(cfaq.sf)[ix , 'Y'],
-#plot(st_coordinates(cfaq.sf)[,2] ~ st_coordinates(cfaq.sf)[,1],pch=20, col="blue")#, add = TRUE)
-text(cfaq.sf[ix, "X"], cfaq.sf[ix, "Y"],
+     sub="Positive: green; Negative: red; Black dots: severe over-predictions")
+points(cfaq[ix, "Y"] ~ cfaq[ix, "X"], 
+       #st_coordinates(cfaq.sf)[ix, 'Y'] ~ st_coordinates(cfaq.sf)[ix , 'X'],
+       pch=20)
+text(cfaq[ix, "X"], cfaq[ix, "Y"],
      #st_coordinates(cfaq.sf)[ix, 'X'], st_coordinates(cfaq.sf)[ix ,'Y'],
      round(sres.ts2[ix], 2), pos=4)
 grid()
 
-#--  create 2km grid
+#--  create grid
+#(n.col <- length(seq.e <- seq(min.x <- floor(min(st_coordinates(cfaq.sf)[,1])/1000)*1000,
+#                              max.x <- ceiling(max(st_coordinates(cfaq.sf)[,1])/1000)*1000, by=1000)))
+(n.col <- length(seq.e <- seq(min.x <- floor(min(cfaq$X)/1000)*1000,
+                              max.x <- ceiling(max(cfaq$X)/1000)*1000, by=1000)))
 #(n.col <- length(seq.e <- seq(min.x <- floor(min(st_coordinates(cfaq.sf)[,2])/1000)*1000,
 #                              max.x <- ceiling(max(st_coordinates(cfaq.sf)[,2])/1000)*1000, by=1000)))
-#(n.row <- length(seq.n <- seq(min.y <- floor(min(st_coordinates(cfaq.sf)[,1])/1000)*1000,
-#                              max.y <- ceiling(max(st_coordinates(cfaq.sf)[,1])/1000)*1000, by=1000)))
+(n.row <- length(seq.n <- seq(min.y <- floor(min(cfaq$Y)/1000)*1000,
+                              max.y <- ceiling(max(cfaq$Y)/1000)*1000, by=1000)))
 
-(n.col <- length(seq.e <- seq(min.x <- floor(min(cfaq.sf$X)/1000)*1000,
-                              max.x <- ceiling(max(cfaq.sf$X)/1000)*1000, by=1000)))
-
-(n.row <- length(seq.n <- seq(min.y <- floor(min(cfaq.sf$Y)/1000)*1000,
-                              max.y <- ceiling(max(cfaq.sf$Y)/1000)*1000, by=1000)))
-
-
-#we want a 500m grid
+#we want a XXXXm grid
 grid <- rast(nrows = n.row, ncols = n.col,
-                xmin=min.x, xmax=max.x,
-                ymin=min.y, ymax=max.y, crs = st_crs(cfaq.sf)$proj4string,
-                resolution = 500, names="waterLevel")
+             xmin=min.x, xmax=max.x,
+             ymin=min.y, ymax=max.y, crs = st_crs(cfaq.sf)$proj4string,
+             resolution = 500, names="waterLevel")
 
 values(grid) <- NA_real_
-class(grid)
 
-crs(grid)
-
-plot(grid); grid()
-
+#class(grid)
+#crs(grid)
+#plot(grid); grid()
 
 grid.df <- as.data.frame(grid, xy = TRUE, na.rm = FALSE)
 names(grid.df)[1:2] <- c("X", "Y") # match the names of the point dataset
 summary(grid.df)
 
-
 pred.ts2 <- predict.lm(model.ts2,
                        newdata = grid.df,
                        interval = "prediction", level = 0.95)
 summary(pred.ts2)
-
 
 class(pred.ts2)
 
@@ -161,13 +143,53 @@ summary(values(grid))
 
 summary(pred.ts2[,"fit"])
 
+#-- add the best fit predictions to the grid
 values(grid) <- pred.ts2[,"fit"]
 summary(values(grid))
 
+#- plot
+plot(grid, col = rainbow(100), main = "OLS 2nd order predicted surface")
+points(st_coordinates(cfaq.sf)[,2] ~ st_coordinates(cfaq.sf)[,1], pch=21, #pch=16,
+       #col = ifelse(res.ts2 < set_units(0, m), "red", "green"),
+       #cex=2*abs(res.ts2)/max(abs(res.ts2)))
+       bg = adjustcolor("black", alpha.f = 0.2),
+       col = "black",
+       cex = 0.9, lwd = 0.5)
 
-plot(grid, main = "OLS 2nd order predicted surface")
+#-- compare 1st-order
+pred.ts1 <- predict.lm(model.ts1,
+                       newdata = grid.df,
+                       interval = "prediction", level = 0.95)
+gridb <- grid
+values(gridb) <- pred.ts1[,"fit"]
+
+# Plot the first grid
+plot(gridb, col = rainbow(100), main = "OLS 1st-order predicted surface")
+points(st_coordinates(cfaq.sf)[,2] ~ st_coordinates(cfaq.sf)[,1], pch=21,
+       bg = adjustcolor("black", alpha.f = 0.2),
+       col = "black",
+       cex = 0.9, lwd = 0.5)
+
+summary(pred.ts2)
+#pred.ts2
+summary(grid.df)
+#grid.df
+
+#-- add the three prediction fields (fit, lower, upper) to the data
+grid.df[, 3:5] <- pred.ts2
+names(grid.df)[3:5] <- c("ts2.fit", "ts2.lwr", "ts2.upr")
+summary(grid.df)
+
+#-- sumarize the uncertainty from the trend surface, as absolute differences between the upper and lower prediction limits
+summary(grid.df$ts2.diff.range <- grid.df$ts2.upr - grid.df$ts2.lwr)
+#-- the above as a percentage of the best fit value
+summary(100*grid.df$ts2.diff.range/grid.df$ts2.fit)
+
+#-- plot: prediction interval of the trend surface
+grid.diff <- grid
+values(grid.diff) <- grid.df$ts2.diff.range
+plot(grid.diff, col=cm.colors(64),
+     main="Range of 95% prediction interval, 2nd-order trend, OLS fit")
+grid()
 points(st_coordinates(cfaq.sf)[,2] ~ st_coordinates(cfaq.sf)[,1], pch=16,
-       col = ifelse(res.ts2 < set_units(0, m), "red", "green"),
-       cex=2*abs(res.ts2)/max(abs(res.ts2)))
-
-
+       col = "gray")
